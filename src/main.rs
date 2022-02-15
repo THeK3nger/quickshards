77d74f81d06f5f44508b7b6fa6801d73c2af8990
Davide Aversa <thek3nger@gmail.com>
2022-02-15 10:44:25 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::{env, fs::OpenOptions, io::Write, path::Path};
+
+use chrono;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Settings {
+    daily_path: String,
+    daily_format: Option<String>,
+}
+
+fn main() {
+    match envy::prefixed("QSHARD_").from_env::<Settings>() {
+        Ok(config) => {
+            let daily_format = config.daily_format.unwrap_or("%Y-%m-%d".into());
+            let date = chrono::offset::Local::now();
+            let daily_file = format!("{0}.md", date.format(&daily_format));
+            let dailiy_file_path = Path::new(&config.daily_path).join(daily_file);
+
+            // Assemble the message
+            let timestamp = date.format("%H:%M");
+            let message = env::args().collect::<Vec<String>>()[1..].join(" ");
+            let total_message = format!("\n- {}\n\t- {}", timestamp, message);
+
+            // Append the message at the end of the file.
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(dailiy_file_path)
+                .unwrap();
+
+            if let Err(e) = writeln!(file, "{}", total_message) {
+                eprintln!("Couldn't write to file: {}", e);
+            }
+        }
+        Err(error) => panic!("{:#?}", error),
+    }
+}
