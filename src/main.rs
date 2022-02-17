fe3c3e10f06940ccf348a74ab3c185bcddae46e4
Davide Aversa <thek3nger@gmail.com>
2022-02-17 19:13:43 +0100

    feat: add better error message if there is no daily note

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
                 .unwrap_or_else(|| String::from("%Y-%m-%d"));
             let date = chrono::offset::Local::now();
             let daily_file = format!("{0}.md", date.format(&daily_format));
-            let dailiy_file_path = Path::new(&config.daily_path).join(daily_file);
+            let daily_file_path = Path::new(&config.daily_path).join(&daily_file);
 
             // Assemble the message
             let timestamp = date.format("%H:%M");
@@ -24,11 +24,15 @@ fn main() {
             let total_message = format!("\n- {}\n\t- {}", timestamp, message);
 
             // Append the message at the end of the file.
-            let mut file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .open(dailiy_file_path)
-                .unwrap();
+            let mut file = match OpenOptions::new().append(true).open(daily_file_path) {
+                Ok(f) => f,
+                Err(err) => panic!(
+                    "Cannot open file '{:?}'. Are you sure you have created it?\n
+                    QS will not create a new file because you may want to use a template for that! :)\n
+                    ERROR IS: {:#?}",
+                    daily_file, err
+                ),
+            };
 
             if let Err(e) = writeln!(file, "{}", total_message) {
                 eprintln!("Couldn't write to file: {}", e);
