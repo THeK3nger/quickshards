7e8d16c0839c345520048d409e818876308f3f4c
Davide Aversa <thek3nger@gmail.com>
2022-05-17 12:09:27 +0200

    refactor: better organization for the code

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,139 @@ struct Cli {
     text: Option<String>,
 }
 
+/// Main Application Class
+struct QuickShards {
+    config: Settings,
+    cli: Cli,
+}
+
+impl QuickShards {
+    /// Create a new QuickShards object.
+    fn new(config: Settings, cli: Cli) -> QuickShards {
+        QuickShards { config, cli }
+    }
+
+    /// Run the main flow of the application.
+    fn run(&self) {
+        let text_editor = &self.config.text_editor.clone();
+        let daily_format = &self
+            .config
+            .daily_format
+            .clone()
+            .unwrap_or_else(|| "%Y-%m-%d".to_owned());
+        let obsidian_vault_path = &self.config.text_editor.clone();
+
+        if self.cli.edit_settings {
+            edit_configuration_file(text_editor);
+            exit(0)
+        }
+
+        let mut message = if !self.cli.interactive {
+            self.cli.text.as_ref().unwrap().to_owned()
+        } else {
+            QuickShards::interactive_editor(text_editor)
+        };
+
+        message = self.handle_tags(message);
+
+        if self.cli.working_memory {
+            let working_memory_path = Path::new(obsidian_vault_path)
+                .join(&self.config.working_memory_file_path.as_ref().unwrap());
+            // Append the message at the end of the file.
+            let mut file = match OpenOptions::new().append(true).open(&working_memory_path) {
+                Ok(f) => f,
+                Err(err) => panic!(
+                    "Cannot open file '{:?}'. ERROR IS: {:#?}",
+                    working_memory_path, err
+                ),
+            };
+            QuickShards::append_line(&mut file, &message);
+            exit(0);
+        }
+
+        let date = chrono::offset::Local::now();
+        let daily_file = format!("{0}.md", date.format(daily_format));
+        let daily_file_path = Path::new(&self.config.obsidian_vault_path)
+            .join(&self.config.daily_path)
+            .join(&daily_file);
+
+        // Assemble the message
+        let timestamp = date.format("%H:%M");
+
+        // Append the message at the end of the file.
+        let mut file = match OpenOptions::new().append(true).open(daily_file_path) {
+                    Ok(f) => f,
+                    Err(err) => panic!(
+                        "Cannot open file '{:?}'. Are you sure you have created it?\n
+                        QS will not create a new file because you may want to use a template for that! :)\n
+                        ERROR IS: {:#?}",
+                        daily_file, err
+                    ),
+                };
+
+        QuickShards::append_log_line(&mut file, &timestamp.to_string(), &message)
+    }
+
+    /// Handle the tagging system for the current message.
+    fn handle_tags(&self, message: String) -> String {
+        for tag in &self.config.tags {
+            if message.starts_with(&tag.tag) {
+                return format!("{}:: {}", tag.value, message.replace(&tag.tag, ""));
+            }
+        }
+        message
+    }
+
+    /// Append a new line in log format.
+    fn append_log_line(file: &mut File, timestamp: &str, entry: &str) {
+        let total_message_body = entry
+            .lines()
+            .filter(|&x| !x.is_empty())
+            .map(|x| format!("\t- {}", x))
+            .collect::<Vec<String>>();
+
+        let total_message = format!("\n- {}\n{}", timestamp, total_message_body.join("\n"));
+
+        if let Err(e) = writeln!(file, "{}", total_message) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+    }
+
+    /// Append a new line with no customizaiton.
+    fn append_line(file: &mut File, entry: &str) {
+        let message = format!("- {}", entry);
+
+        if let Err(e) = writeln!(file, "{}", message) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+    }
+
+    /// Open an external editor to edit the entry message.
+    fn interactive_editor(editor: &str) -> String {
+        let tmp_dir = temp_dir();
+        let filename = format!("{}.md", Uuid::new_v4());
+        let tmp_file_path = Path::new(&tmp_dir).join(&filename);
+
+        Command::new(editor)
+            .arg(&tmp_file_path)
+            .spawn()
+            .expect("Cannot open VIM.")
+            .wait()
+            .unwrap();
+
+        match fs::read_to_string(&tmp_file_path) {
+            Ok(config_str) => config_str,
+            Err(err) => {
+                panic!(
+                    "Cannot open file '{:?}'. ERROR IS: {:#?}",
+                    tmp_file_path, err
+                )
+            }
+        }
+    }
+}
+
+/// Open an external editor to edit the configuration file.
 fn edit_configuration_file(editor: &str) {
     let config_dir = dirs::config_dir().unwrap().join("QuickShards");
     fs::create_dir_all(&config_dir).unwrap();
@@ -65,6 +198,7 @@ fn edit_configuration_file(editor: &str) {
         .unwrap();
 }
 
+/// Load the configuration file.
 fn load_configuration_file() -> Settings {
     let config_dir = dirs::config_dir().unwrap().join("QuickShards");
     fs::create_dir_all(&config_dir).unwrap();
@@ -83,114 +217,9 @@ fn load_configuration_file() -> Settings {
     }
 }
 
-fn handle_tags(tags: &Vec<Tag>, message: String) -> String {
-    for tag in tags {
-        if message.starts_with(&tag.tag) {
-            return format!("{}:: {}", tag.value, message.replace(&tag.tag, ""));
-        }
-    }
-    message
-}
-
-fn append_log_line(file: &mut File, timestamp: &str, entry: &str) {
-    let total_message_body = entry
-        .lines()
-        .filter(|&x| !x.is_empty())
-        .map(|x| format!("\t- {}", x))
-        .collect::<Vec<String>>();
-
-    let total_message = format!("\n- {}\n{}", timestamp, total_message_body.join("\n"));
-
-    if let Err(e) = writeln!(file, "{}", total_message) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
-}
-
-fn append_line(file: &mut File, entry: &str) {
-    let message = format!("- {}", entry);
-
-    if let Err(e) = writeln!(file, "{}", message) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
-}
-
-fn interactive_editor(editor: &str) -> String {
-    let tmp_dir = temp_dir();
-    let filename = format!("{}.md", Uuid::new_v4());
-    let tmp_file_path = Path::new(&tmp_dir).join(&filename);
-
-    Command::new(editor)
-        .arg(&tmp_file_path)
-        .spawn()
-        .expect("Cannot open VIM.")
-        .wait()
-        .unwrap();
-
-    match fs::read_to_string(&tmp_file_path) {
-        Ok(config_str) => config_str,
-        Err(err) => {
-            panic!(
-                "Cannot open file '{:?}'. ERROR IS: {:#?}",
-                tmp_file_path, err
-            )
-        }
-    }
-}
-
 fn main() {
     let cli = Cli::parse();
     let config = load_configuration_file();
-
-    if cli.edit_settings {
-        edit_configuration_file(&config.text_editor);
-        exit(0)
-    }
-
-    let mut message = if !cli.interactive {
-        cli.text.unwrap()
-    } else {
-        interactive_editor(&config.text_editor)
-    };
-
-    message = handle_tags(&config.tags, message);
-
-    if cli.working_memory {
-        let working_memory_path =
-            Path::new(&config.obsidian_vault_path).join(&config.working_memory_file_path.unwrap());
-        // Append the message at the end of the file.
-        let mut file = match OpenOptions::new().append(true).open(&working_memory_path) {
-            Ok(f) => f,
-            Err(err) => panic!(
-                "Cannot open file '{:?}'. ERROR IS: {:#?}",
-                working_memory_path, err
-            ),
-        };
-        append_line(&mut file, &message);
-        exit(0);
-    }
-
-    let daily_format = config
-        .daily_format
-        .unwrap_or_else(|| String::from("%Y-%m-%d"));
-    let date = chrono::offset::Local::now();
-    let daily_file = format!("{0}.md", date.format(&daily_format));
-    let daily_file_path = Path::new(&config.obsidian_vault_path)
-        .join(&config.daily_path)
-        .join(&daily_file);
-
-    // Assemble the message
-    let timestamp = date.format("%H:%M");
-
-    // Append the message at the end of the file.
-    let mut file = match OpenOptions::new().append(true).open(daily_file_path) {
-                Ok(f) => f,
-                Err(err) => panic!(
-                    "Cannot open file '{:?}'. Are you sure you have created it?\n
-                    QS will not create a new file because you may want to use a template for that! :)\n
-                    ERROR IS: {:#?}",
-                    daily_file, err
-                ),
-            };
-
-    append_log_line(&mut file, &timestamp.to_string(), &message)
+    let qs = QuickShards::new(config, cli);
+    qs.run();
 }
